8b59ef9b22fbcb47d05b8525e14ea7212659a4f3
aleilias <86530708+aleilias@users.noreply.github.com>
2021-12-26 11:45:44 -0600

    react components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import UList from "./UList";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <Heading />
+    <UList />
   </div>,
   document.getElementById("root")
 );
